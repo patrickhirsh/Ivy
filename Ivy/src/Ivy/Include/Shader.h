cbe5ce2cf120abda9fcaa085f08b7b6637639ea3
Patrick Hirsh <hirshpatrick@gmail.com>
2020-01-25 22:12:13 -0700

    Abstracted shaders

---
+++ Ivy/src/Ivy/Include/Shader.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "IvyPCH.h"
+
+namespace _Ivy
+{
+	class Shader
+	{
+	public:
+		static Ivy::Ref<Shader> Create			(GLuint shaderType, std::string shaderPath);
+		static void Bind						(const std::vector<Ivy::Ref<Shader>>& shaders);
+		static void Unbind						();
+
+	private:
+		static GLuint							_activeProgram;
+		static std::vector<Ivy::Ref<Shader>>	_activeShaders;
+
+	public:
+		~Shader									() {};
+		void Compile							();
+	private:
+		Shader									() {};
+
+		GLuint									_id;
+		GLuint									_type;
+		std::string								_path;
+	};
+}
